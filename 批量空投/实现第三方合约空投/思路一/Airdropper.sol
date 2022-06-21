57097c77a7c5aa905bd089d24e6a847341c6c523
xudan <xudan@apowo.com>
2022-06-21 17:17:14 +0800

    update

--- 批量空投/实现第三方合约空投/思路一/Airdropper.sol
+++ 批量空投/实现第三方合约空投/思路一/Airdropper.sol
@@ -1,2 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.2;
+
+contract Airdrop {
+    bytes public data;
+
+    function batch(address tokenAddr, address[] calldata toAddr, uint256 [] calldata value) public returns (bool){
+        
+        require(toAddr.length == value.length && toAddr.length >= 1);
+        
+        for(uint256 i = 0 ; i < toAddr.length; i++){
+    
+            (bool success, bytes memory _data) = tokenAddr.call(
+                abi.encodeWithSignature("transferFrom(address,address,uint256)",  msg.sender, toAddr[i], value[i])
+            );
+            require(success, "call falled");
+            data = _data;
+        }
+        return true;
+    }
+}
