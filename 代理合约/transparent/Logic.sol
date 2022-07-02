eb4c821919611757827bc830eabc510421f96382
dan <875126243@qq.com>
2022-07-03 00:19:54 +0800

    fix logic

--- 代理合约/transparent/Logic.sol
+++ 代理合约/transparent/Logic.sol
@@ -8,4 +8,18 @@ contract MyContractProxyTest is Initializable {
     constructor() {
         _disableInitializers();
     }
+
+    mapping(string => uint256) private mappingArr;
+
+    event logs(string indexed _key,uint256 _value);
+
+    function setMappingArr(string memory _key,uint256 _value) external{
+        mappingArr[_key] = _value;
+        emit logs(_key,_value);
+    }
+
+
+    function getMappingArr(string memory _key)public view returns(uint256){
+        return mappingArr[_key];
+    }
 }
